75e92ef81bc9b245e24ddc5f4f1b030981fdf56a
CBW2007 <2337118038@qq.com>
2021-02-27 13:26:49 +0800

    feat: add xml styelsheet into utils

--- src/CreateSitemapStylesheet.js
+++ src/CreateSitemapStylesheet.js
@@ -2,13 +2,9 @@ import * as utils from './utils'
 import path from 'path'
 
 export default async function CreateSitemapStylesheet (siteUrl, pathPrefix) {
-  const siteRegex = /(\{\{blog-url\}\})/g
-
-  // Get our stylesheet template
-  const data = await utils.readFile('./sitemap.xsl')
 
   // Replace the `{{blog-url}}` variable with our real site URL
-  const sitemapStylesheet = data.toString().replace(siteRegex, new URL(path.join(pathPrefix, 'sitemap.xml'), siteUrl).toString())
+  const sitemapStylesheet = utils.xmlStylesheetTemplate(new URL(path.join(pathPrefix, 'sitemap.xml'), siteUrl).toString())
 
   // Save the updated stylesheet to the public folder, so it will be
   // available for the xml sitemap files
