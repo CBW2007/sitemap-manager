14a1238d1a5e06ffb24fdfe93dabfc16d991f0dc
CBW2007 <2337118038@qq.com>
2021-03-07 20:53:23 +0800

    chore(tests): add test script

---
+++ scripts/test.js
@@ -0,0 +1,237 @@
+const { CreateSitemap, CreateIndexSitemap, CreateSitemapStylesheet } = require('../dist/index')
+const { mkDir, rmDir, exists } = require('../dist/utils')
+const path = require('path')
+
+const data=JSON.parse(`
+{
+    "data": {
+        "site": {
+            "siteMetadata": {
+                "siteUrl": "https://example.com"
+            }
+        },
+        "postsQuery": {
+            "edges": [{
+                "node": {
+                    "id": "00a79c3f-0ec9-504e-9b32-28a1f7e0b77a",
+                    "fields": {
+                        "slug": "/test/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "ef47f21b-2e7e-58ae-9f70-43b408188969",
+                    "fields": {
+                        "slug": "/blog/one/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "a8327a61-e913-5d9b-a21a-48c68fe0628e",
+                    "fields": {
+                        "slug": "/blog/two/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "779744c7-13e6-599b-9d4b-deba11f0ecd3",
+                    "fields": {
+                        "slug": "/empty/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "2ee42eea-58de-5a56-846c-985f79be3c9b",
+                    "fields": {
+                        "slug": "/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "8e823adf-5de6-5a6f-81e3-559faddda74b",
+                    "fields": {
+                        "slug": "/math/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "2b123280-0275-54d5-8c5c-7ae5b061fba5",
+                    "fields": {
+                        "slug": "/math/poly/div-mod/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "d923ddcc-0e6c-5cc7-88cd-66cf38aefa79",
+                    "fields": {
+                        "slug": "/math/poly/intro/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "d95c2b31-f6bd-52bc-8078-8b7f5c5c9912",
+                    "fields": {
+                        "slug": "/math/poly/fft/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "e50d721e-5570-50dc-8089-dde25e0727dc",
+                    "fields": {
+                        "slug": "/math/poly/fwt/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "9bc90933-d44f-57fe-b36d-1c6ee5d5477e",
+                    "fields": {
+                        "slug": "/math/poly/inv-tri-func/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "c4bb4bf7-5b67-525c-b03b-1e653f08df1a",
+                    "fields": {
+                        "slug": "/math/poly/inv/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "d02a27a1-7a5d-5722-805b-349185305c8c",
+                    "fields": {
+                        "slug": "/math/poly/lagrange/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "492c557d-6279-51e9-9e4d-300114300382",
+                    "fields": {
+                        "slug": "/math/poly/multipoint-eval-interpolation/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "8e9b4fe1-6ad9-5917-a948-1cc9c875b523",
+                    "fields": {
+                        "slug": "/math/poly/ln-exp/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "23778dce-fb19-5569-ada1-28247a506162",
+                    "fields": {
+                        "slug": "/math/poly/newton/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "f71fe369-5225-50bc-8c04-28a882844abb",
+                    "fields": {
+                        "slug": "/math/poly/ntt/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "e032c60c-ab98-5a7c-9686-ffbf89018d2a",
+                    "fields": {
+                        "slug": "/math/poly/sqrt/"
+                    }
+                }
+            }, {
+                "node": {
+                    "id": "a9ba831f-b536-5cb9-a7de-1b04b3e82dcf",
+                    "fields": {
+                        "slug": "/math/poly/tri-func/"
+                    }
+                }
+            }]
+        },
+        "tagsQuery": {
+            "group": [{
+                "fieldValue": "Chicago"
+            }, {
+                "fieldValue": "Chinese"
+            }, {
+                "fieldValue": "TEST"
+            }, {
+                "fieldValue": "WIP"
+            }, {
+                "fieldValue": "animals"
+            }, {
+                "fieldValue": "math"
+            }, {
+                "fieldValue": "polynomial"
+            }, {
+                "fieldValue": "zoos"
+            }, {
+                "fieldValue": "中文"
+            }]
+        }
+    }
+}`).data
+
+const siteURL = data.site.siteMetadata.siteUrl
+const pathPrefix = ''
+const reporter = {
+  panicOnBuild: (msg) => {console.log(msg)}
+}
+
+const init = async () => {
+  if (await exists('public'))
+  {
+    console.log("delete files from previous test")
+    await rmDir('public', {
+      recursive: true
+    })
+  }
+  console.log('create public folder')
+  await mkDir('public')
+}
+const main = () => {
+  console.log('create sitemaps')
+  CreateSitemap(
+    path.resolve('./public/sitemap-articles.xml'),
+    pathPrefix,
+    siteURL,
+    data.postsQuery.edges,
+    (data) => { return data.node.fields.slug },
+    reporter,
+  )
+  CreateSitemap(
+    path.resolve('./public/sitemap-logs.xml'),
+    pathPrefix,
+    siteURL,
+    data.postsQuery.edges,
+    (data) => { return data.node.fields.slug + 'changelog/' },
+    reporter,
+  )
+  CreateSitemap(
+    path.resolve('./public/sitemap-tags.xml'),
+    pathPrefix,
+    siteURL,
+    data.tagsQuery.group,
+    (data) => { return `/tags/${data.fieldValue}/` },
+    reporter,
+  )
+  CreateSitemap(
+    path.resolve('./public/sitemap-pages.xml'),
+    pathPrefix,
+    siteURL,
+    ['/pages/', '/settings/'],
+    (data) => { return data },
+    reporter,
+  )
+  CreateIndexSitemap(
+    path.resolve('./public/sitemap.xml'),
+    pathPrefix,
+    siteURL,
+    ['sitemap-pages.xml', 'sitemap-articles.xml', 'sitemap-logs.xml', 'sitemap-tags.xml'],
+    reporter,
+  )
+  CreateSitemapStylesheet(
+    path.resolve('./public/sitemap.xsl'),
+    pathPrefix,
+    siteURL,
+  )
+}
+init().then(main).catch((e) => { console.error(e) })
--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,9 @@ import pify from 'pify'
 
 export const writeFile = pify(fs.writeFile)
 export const readFile = pify(fs.readFile)
+export const mkDir = pify(fs.mkdir)
+export const rmDir = pify(fs.rmdir)
+export const exists = fs.existsSync
 
 export const sitemapTemplate = (data) => { return `<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="sitemap.xsl"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">${data}</urlset>` }
 export const sitemapIndexTemplate = (data) => { return `<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="sitemap.xsl"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${data}</sitemapindex>` }
