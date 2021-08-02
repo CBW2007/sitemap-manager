e41ec6915540f9480624d905f3d924053abdb783
CBW2007 <2337118038@qq.com>
2021-08-02 15:18:50 +0800

    style: run eslint for scripts

--- scripts/test.js
+++ scripts/test.js
@@ -1,23 +1,22 @@
 const { SitemapManager } = require('../dist/index')
 const fs = require('fs')
 
-if (fs.existsSync('./public'))
-    fs.rmdirSync('./public',{recursive: true})
+if (fs.existsSync('./public')) { fs.rmdirSync('./public', { recursive: true }) }
 fs.mkdirSync('./public')
 
 const MySitemap = new SitemapManager({
-    siteURL: 'https://example.com/',
-    pathPrefix: 'aaa'
+  siteURL: 'https://example.com/',
+  pathPrefix: 'aaa'
 })
 
-MySitemap.addUrl('aa',[
-    {loc: 'https://example.com/aaa/a/'},
-    {loc: 'https://example.com/aaa/中文/'},
-    {loc: 'https://example.com/aaa/b/', lastmod: new Date()},
-    {loc: 'https://example.com/aaa/c/', lastmod: '2021-07-01T08:00:00'},
+MySitemap.addUrl('aa', [
+  { loc: 'https://example.com/aaa/a/' },
+  { loc: 'https://example.com/aaa/中文/' },
+  { loc: 'https://example.com/aaa/b/', lastmod: new Date() },
+  { loc: 'https://example.com/aaa/c/', lastmod: '2021-07-01T08:00:00' }
 ])
 
-MySitemap.addUrl('bb',[{loc: 'https://example.com/aaa/ab/'}])
-MySitemap.addUrl('bb',[{loc: 'https://example.com/aaa/ac/'}])
+MySitemap.addUrl('bb', [{ loc: 'https://example.com/aaa/ab/' }])
+MySitemap.addUrl('bb', [{ loc: 'https://example.com/aaa/ac/' }])
 
 MySitemap.finish()
